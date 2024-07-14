43a272a47bef4b8dd0f5b42fd342ef4511e73bea
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-07-14 11:38:53 +0300

    change .json

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { InfinitySpin } from "react-loader-spinner";
+import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader: React.FC = () => {
   return (
     <div>
       <InfinitySpin
-        visible={true}
-        width="200"
         color="#0056b3"
-        ariaLabel="infinity-spin-loading"
       />
     </div>
   );
